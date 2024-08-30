a650058893159303c28c5193ab2bb43ec0610482
NoSpawnn <78027161+NoSpawnn@users.noreply.github.com>
2024-08-30 17:07:05 +0100

    Adjust config

--- src/main/java/com/nospawnn/githubactivitytracker/web/Web.java
+++ src/main/java/com/nospawnn/githubactivitytracker/web/Web.java
@@ -5,8 +5,16 @@ import com.renomad.minum.web.FullSystem;
 public class Web {
     public static void run() {
         var fs = FullSystem.initialize();
+        var context = fs.getContext();
+        var constants = context.getConstants();
+        
+        new PathRegister(context).registerDomains();
 
-        new PathRegister(fs.getContext()).registerDomains();
+        System.out.printf("\nServer running, access at Access at http://%s:%d or https://%s:%d\n",
+                constants.hostName,
+                constants.serverPort,
+                constants.hostName,
+                constants.secureServerPort);
 
         fs.block();
     }
